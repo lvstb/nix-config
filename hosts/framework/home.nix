921c212ed102d3c0683f674c357d8777cb2a6569
lvstb <lars@wingu.dev>
2024-12-18 21:54:13 +0100

    gtk update

--- hosts/framework/home.nix
+++ hosts/framework/home.nix
@@ -78,18 +78,18 @@ in
 
   # Styling
   stylix.targets.waybar.enable = false;
-  gtk = {
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-  };
+  # gtk = {
+  #   iconTheme = {
+  #     name = "Papirus-Dark";
+  #     package = pkgs.papirus-icon-theme;
+  #   };
+  #   gtk3.extraConfig = {
+  #     gtk-application-prefer-dark-theme = 1;
+  #   };
+  #   gtk4.extraConfig = {
+  #     gtk-application-prefer-dark-theme = 1;
+  #   };
+  # };
   qt = {
     enable = true;
     style.name = "adwaita-dark";
