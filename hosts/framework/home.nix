b9cd8537055bb4d4dac6d90a53c6289cf8975cc2
lvstb <lars@wingu.dev>
2024-12-18 22:24:32 +0100

    test

--- hosts/framework/home.nix
+++ hosts/framework/home.nix
@@ -13,33 +13,33 @@ in
 
     file = {
       # Hyprland Config
-      ".config/hypr".source = dotfiles/.config/hypr;
+      ".config/hypr".source = ../../dotfiles/.config/hypr;
 
       # wlogout icons
-      ".config/wlogout/icons".source = config/wlogout;
+      ".config/wlogout/icons".source = ../../config/wlogout;
 
       # Top Level Files symlinks
-      ".zshrc".source = dotfiles/.zshrc;
-      ".zshenv".source = dotfiles/.zshenv;
-      ".xinitrc".source = dotfiles/.xinitrc;
-      ".gitconfig".source = dotfiles/.gitconfig;
-      ".ideavimrc".source = dotfiles/.ideavimrc;
-      ".nirc".source = dotfiles/.nirc;
-      ".local/bin/wallpaper".source = dotfiles/.local/bin/wallpaper;
+      ".zshrc".source = ../../dotfiles/.zshrc;
+      ".zshenv".source = ../../dotfiles/.zshenv;
+      ".xinitrc".source = ../../dotfiles/.xinitrc;
+      ".gitconfig".source = ../../dotfiles/.gitconfig;
+      ".ideavimrc".source = ../../dotfiles/.ideavimrc;
+      ".nirc".source = ../../dotfiles/.nirc;
+      ".local/bin/wallpaper".source = ../../dotfiles/.local/bin/wallpaper;
 
       # Config directories
-      # ".config/dunst".source = dotfiles/.config/dunst;
-      ".config/fastfetch".source = dotfiles/.config/fastfetch;
-      ".config/kitty".source = dotfiles/.config/kitty;
-      ".config/mpv".source = dotfiles/.config/mpv;
-      ".config/tmux/tmux.conf".source = dotfiles/.config/tmux/tmux.conf;
-      ".config/waybar".source = dotfiles/.config/waybar;
-      ".config/yazi".source = dotfiles/.config/yazi;
+      ".config/dunst".source = ../../dotfiles/.config/dunst;
+      ".config/fastfetch".source = ../../dotfiles/.config/fastfetch;
+      ".config/kitty".source = ../../dotfiles/.config/kitty;
+      ".config/mpv".source = ../../dotfiles/.config/mpv;
+      ".config/tmux/tmux.conf".source = ../../dotfiles/.config/tmux/tmux.conf;
+      ".config/waybar".source = ../../dotfiles/.config/waybar;
+      ".config/yazi".source = ../../dotfiles/.config/yazi;
 
       # Individual config files
-      ".config/kwalletrc".source = dotfiles/.config/kwalletrc;
-      ".config/starship.toml".source = dotfiles/.config/starship.toml;
-      ".config/mimeapps.list".source = dotfiles/.config/mimeapps.list;
+      ".config/kwalletrc".source = ../../dotfiles/.config/kwalletrc;
+      ".config/starship.toml".source = ../../dotfiles/.config/starship.toml;
+      ".config/mimeapps.list".source = ../../dotfiles/.config/mimeapps.list;
     };
 
     # sessionVariables = {
