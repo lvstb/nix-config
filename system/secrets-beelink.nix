e26905e083624a4a07565ff793993b49f2259240
Lars Van Steenbergen <lars@wingu.dev>
2025-07-16 22:06:48 +0200

    update hardware config

--- system/secrets-beelink.nix
+++ system/secrets-beelink.nix
@@ -2,18 +2,18 @@
 { config, ... }: {
   imports = [ ./secrets.nix ];
   
-  sops.secrets = {
-    # WiFi password (needed)
-    wifi_home_password = {
-      sopsFile = ../secrets/beelink.yaml;
-      key = "wifi/home_password";
-      owner = "root";
-      group = "wheel";
-      mode = "0400";
-    };
+#  sops.secrets = {
+#    # WiFi password (needed)
+#    wifi_home_password = {
+#      sopsFile = ../secrets/beelink.yaml;
+#      key = "wifi/home_password";
+#      owner = "root";
+#      group = "wheel";
+#      mode = "0400";
+#    };
     
     # Add other secrets here when actually needed
     # server_api_key = { ... };
     # database_password = { ... };
-  };
+#  };
 }
